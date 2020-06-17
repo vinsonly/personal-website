13fe5e6e27a42b39a73da961c132351367cae233
Vinson FM Ly <vinsonly@Vinsons-MacBook-Pro.local>
2020-06-17 12:11:19 -0700

    reverted to using sendinblue smtp service

--- emailTransporter.js
+++ emailTransporter.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL
+    host: 'smtp-relay.sendinblue.com',
+    port: 587,
+    secure: false, // use SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS 
